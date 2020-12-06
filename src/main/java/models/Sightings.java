1e2261a3208839d6eb18a2123433123aab8a303d
Joseph-beep-tech <Josephcluff006@gmail.com>
2020-12-06 21:01:41 +0300

    instances created

--- src/main/java/models/Sightings.java
+++ src/main/java/models/Sightings.java
@@ -1,9 +1,20 @@
 package models;
 
 
+import java.security.Timestamp;
+
 public class Sightings{
     private String location;
     private String rangerName;
     private int wildlifeId;
     private int id;
+
+    private Timestamp time;
+
+    public Sightings(String location, String rangerName, int wildlifeId){
+
+        this.location = location;
+        this.rangerName = rangerName;
+        this.wildlifeId = wildlifeId;
+    }
 }
