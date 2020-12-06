5862ed21d6be5ede2666f200abe9d1437b472f81
Joseph-beep-tech <Josephcluff006@gmail.com>
2020-12-06 12:23:47 +0300

    getters and setters created

--- src/main/java/models/Wildlife.java
+++ src/main/java/models/Wildlife.java
@@ -12,4 +12,28 @@ public abstract class Wildlife{
         this.animalName = animalName;
         this.type = type;
     }
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        Wildlife.id = id;
+    }
+
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    public void setAnimalName(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
